1d77b37e711fe5b122a24d5fa9808d5adf852b94
Szymon Heczko <szymon.heczko@onet.pl>
2025-09-14 01:38:19 +0200

    Added chat on start screen

--- src/frontend/Humi/Humi/ViewModels/StartScreenViewModel.cs
+++ src/frontend/Humi/Humi/ViewModels/StartScreenViewModel.cs
@@ -1,5 +1,10 @@
 using Avalonia.Controls;
 using CommunityToolkit.Mvvm.Input;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView.Painting.Effects;
+using SkiaSharp;
 
 namespace Humi.ViewModels;
 
@@ -11,6 +16,48 @@ public class StartScreenViewModel : ViewModelBase
     {
         StartAnalysisCommand = new RelayCommand(StartAnalysis);
     }
+
+        public ISeries[] Series { get; set; }
+            = new ISeries[] { new LineSeries<int> {
+                Fill = null,
+                Stroke = new SolidColorPaint(new SKColor(101, 143, 100, 255)) { StrokeThickness = 4 },
+                Values = new[] { 55, 69, 71, 83, 4, 90, 10 },
+                GeometryFill = new SolidColorPaint(SKColors.White),
+                GeometryStroke = new SolidColorPaint(new SKColor(101, 143, 100, 255)) { StrokeThickness = 4 }
+                    } ,
+                };
+
+        public Axis[] XAxes { get; set; }
+            = new Axis[]
+            {
+                new Axis
+                {
+                    Labels = ["Neutralny", "Szczęśliwy","Przerażony", "Zły","Zaskoczony", "Smutnt"],
+                    LabelsPaint = new SolidColorPaint(new SKColor(255, 255, 255, 178)),
+                    TextSize = 12,
+                    SeparatorsPaint = new SolidColorPaint(SKColors.LightSlateGray)
+                    {
+                        StrokeThickness = 2,
+                        PathEffect = new DashEffect(new float[] { 2, 2 })
+                    }
+                }
+            };
+
+        public Axis[] YAxes { get; set; }
+            = new Axis[]
+            {
+                new Axis
+                {
+                    MinStep=20,
+                    LabelsPaint = new SolidColorPaint(new SKColor(255, 255, 255, 178)),
+                    TextSize = 12,
+                    SeparatorsPaint = new SolidColorPaint(SKColors.LightSlateGray) 
+                    {
+                        StrokeThickness = 2, 
+                        PathEffect = new DashEffect(new float[] { 2, 2 })
+                    }
+                }
+            };
 
     private void StartAnalysis()
     {
